0d3e67102d38ae26f53a9f205e848da4e5ccd0be
chungnb <chungvp1993@gmail.com>
2026-08-07 10:35:40 +0700

    update

---
+++ resources/views/clients/update.blade.php
@@ -0,0 +1,13 @@
+<h1>Cập nhật sản phẩm</h1>
+
+<form method="POST" action="/products">
+    @csrf
+
+    <input type="text" name="name" placeholder="Tên sản phẩm"><br><br>
+
+    <input type="number" name="price" placeholder="Giá"><br><br>
+
+    <textarea name="description" placeholder="Mô tả"></textarea><br><br>
+
+    <button type="submit">Lưu</button>
+</form>
